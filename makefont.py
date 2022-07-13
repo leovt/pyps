3c034d6f26aba0795198eb4e5178dabb3368380a
leovt <leonhard.vogt@gmx.ch>
2022-07-13 22:03:32 +0200

    template for creating fonts

---
+++ makefont.py
@@ -0,0 +1,16 @@
+'''
+variables in template.ttx
+
+{created_timestamp}
+{avg_char_width}
+{ascent}
+{descent}
+{line_gap}
+{win_ascent}
+{win_descent}
+{family_name}
+{glyph_id_tags}
+{unicode_map_tags}
+{charstring_tags}
+{mtx_tags}
+'''
